6cf76610230ea3558841d5086df2b2a9d3aa2883
xing-6464 <xing_6464@foxmail.com>
2023-02-16 01:30:38 +0800

    first commit

---
+++ lexer/TokenType.js
@@ -0,0 +1,11 @@
+const Enum = require('../common/Enum')
+
+module.exports = {
+  KEYWORD: new Enum('KEYWORD', 1),
+  VARIABLE: new Enum('VARIABLE', 1),
+  OPERATOR: new Enum('KOPERATOR', 1),
+  BRACKET: new Enum('BRACKET', 1),
+  INTEGER: new Enum('INTEGER', 1),
+  FLOAT: new Enum('FLOAT', 1),
+  BOOLEAN: new Enum('BOOLEAN', 1),
+}
